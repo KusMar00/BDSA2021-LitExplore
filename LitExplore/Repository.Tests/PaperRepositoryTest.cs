5f9c472541058bf6db085e0691cbff33054aaaad
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-12-03 16:12:57 +0100

    PaperRepository done

--- LitExplore/Repository.Tests/PaperRepositoryTest.cs
+++ LitExplore/Repository.Tests/PaperRepositoryTest.cs
@@ -18,34 +18,38 @@ public class PaperRepositoryTest : RepositoryTests
         Paper
             Paper_1 = new Paper
             {
-                Id = 0,
+                Id = 1,
                 Authors = new HashSet<Author> { Author_1, Author_2 },
                 Name = "Paper 1",
+                URL = "https://paper-1.com",
                 Abstract = "Abstract 1"
             },
             Paper_2 = new Paper
             {
-                Id = 1,
+                Id = 2,
                 Authors = new HashSet<Author> { Author_3, Author_4 },
                 Name = "Paper 2",
+                URL = "https://paper-2.com",
                 Abstract = "Abstract 2"
             },
             Paper_3 = new Paper
             {
-                Id = 1,
+                Id = 3,
                 Authors = new HashSet<Author> { Author_5 },
                 Name = "Paper 3",
+                URL = "https://paper-3.com",
                 Abstract = "Abstract 3",
-                Citings = new[] { Paper_1, Paper_2 }
+                Citings = new[] { Paper_1 }
             };
 
-        Context.Papers.AddRange(new[] { Paper_1 });
+        Context.Papers.AddRange(new[] { Paper_1, Paper_2, Paper_3 });
 
         Context.SaveChanges();
     }
 
+    #region ReadAsync
     [Fact]
-    public async void Read_Non_Existing_Returns_Null()
+    public async void ReadAsync_Non_Existing_Returns_Null()
     {
         // Arrange
         PaperDTO? expected = null;
@@ -58,7 +62,7 @@ public class PaperRepositoryTest : RepositoryTests
     }
 
     [Fact]
-    public async void Read_Paper_1_Returns_Paper_1()
+    public async void ReadAsync_Paper_1_Returns_Paper_1()
     {
         // Arrange
         PaperDTO expected = new(1, "Paper 1");
@@ -69,4 +73,157 @@ public class PaperRepositoryTest : RepositoryTests
         // Assert
         Assert.Equal(expected, actual);
     }
+    #endregion
+
+    #region ReadDetailsAsync
+    [Fact]
+    public async void ReadDetailsAsync_Non_Existing_Returns_Null()
+    {
+        // Arrange
+        PaperDTO? expected = null;
+
+        // Act
+        var actual = await repo.ReadDetailsAsync(10);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void ReadDetailsAsync_Paper_1_Returns_Paper_1()
+    {
+        // Arrange
+        PaperDetailsDTO expected = new(
+            1,
+            "Paper 1",
+            new HashSet<AuthorDTO> {
+                new(1, "Author 1", "Surname 1"),
+                new(2, "Author 2", "Surname 2")
+            },
+            "https://paper-1.com",
+            "Abstract 1"
+        );
+
+        // Act
+        var actual = await repo.ReadDetailsAsync(1);
+
+        // Assert
+        Assert.NotNull(actual);
+#pragma warning disable CS8602 // Dereference of a possibly null reference. (We already asserted not null)
+        Assert.Equal(expected.Id, actual.Id);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        Assert.Equal(expected.Name, actual.Name);
+        Assert.Equal(expected.Authors, actual.Authors);
+        Assert.Equal(expected.URL, actual.URL);
+        Assert.Equal(expected.Abstract, actual.Abstract);
+    }
+    #endregion
+
+    #region ReadByNameAsync
+    [Fact]
+    public async void ReadByNameAsync_Non_Exsisting_Returns_Empty_List()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { };
+
+        // Act
+        var actual = await repo.ReadByNameAsync("Paper 10");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void ReadByNameAsync_Paper_1_Returns_List_With_Paper_1()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { new(1, "Paper 1") };
+
+        // Act
+        var actual = await repo.ReadByNameAsync("Paper 1");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void ReadByNameAsync_per_Returns_All()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { new(1, "Paper 1"), new(2, "Paper 2"), new(3, "Paper 3") };
+
+        // Act
+        var actual = await repo.ReadByNameAsync("per");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void ReadByNameAsync_Is_Case_Insensitive()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { new(1, "Paper 1"), new(2, "Paper 2"), new(3, "Paper 3") };
+
+        // Act
+        var actual = await repo.ReadByNameAsync("pAPeR");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    #endregion
+    
+    #region ReadByRelationAsync
+    [Fact]
+    public async void ReadByRelationAsync_Paper_2_Has_No_Relations()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { };
+
+        // Act
+        var actual = await repo.ReadByRelationsAsync(2);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void ReadByRelationAsync_Non_Exsisting_Returns_Empty_List()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { };
+
+        // Act
+        var actual = await repo.ReadByRelationsAsync(10);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void ReadByRelationAsync_Paper_1_Cited_By_3()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { new(3, "Paper 3") };
+
+        // Act
+        var actual = await repo.ReadByRelationsAsync(1);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public async void ReadByRelationAsync_Paper_3_Cited_By_1()
+    {
+        // Arrange
+        var expected = new List<PaperDTO>() { new(1, "Paper 1") };
+
+        // Act
+        var actual = await repo.ReadByRelationsAsync(3);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    #endregion
 }
--- LitExplore/Repository/PaperRepository.cs
+++ LitExplore/Repository/PaperRepository.cs
@@ -9,23 +9,44 @@ public class PaperRepository : IPaperRepository
     public async Task<PaperDTO?> ReadAsync(int id)
     {
         var papers = from p in context.Papers
-                    where p.Id == id
-                    select new PaperDTO(p.Id, p.Name);
+                     where p.Id == id
+                     select new PaperDTO(p.Id, p.Name);
         return await papers.FirstOrDefaultAsync();
     }
 
-    public Task<IReadOnlyCollection<PaperDTO>> ReadByNameAsync(string name)
+    public async Task<IReadOnlyCollection<PaperDTO>> ReadByNameAsync(string name)
     {
-        throw new NotImplementedException();
+        var papers = from p in context.Papers
+                     where p.Name != null && p.Name.ToLower().Contains(name.ToLower())
+                     select new PaperDTO(p.Id, p.Name);
+        return (await papers.ToListAsync()).AsReadOnly();
     }
 
-    public Task<IReadOnlyCollection<PaperDTO>> ReadByRelationsAsync(int paperId)
+    public async Task<IReadOnlyCollection<PaperDTO>> ReadByRelationsAsync(int paperId)
     {
-        throw new NotImplementedException();
-    }
+        var papers = from p in context.Papers
+                     // This checks the number of cited/citedby papers whose id matches paperId.
+                     // using .First may be a little faster, but it is slightly less readable.
+                     where p.Citings.Count(p => p.Id == paperId) > 0 || p.CitedBy.Count(p => p.Id == paperId) > 0
+                     select new PaperDTO(p.Id, p.Name);
 
-    public Task<PaperDetailsDTO?> ReadDetailsAsync(int id)
+        return (await papers.ToListAsync()).AsReadOnly();
+    }
+    
+    public async Task<PaperDetailsDTO?> ReadDetailsAsync(int id)
     {
-        throw new NotImplementedException();
+        var papers = from p in context.Papers
+                     where p.Id == id
+                     select new PaperDetailsDTO(
+                         p.Id,
+                         p.Name,
+                         ( // We need to convert Authors to AuthorDTOs.
+                             from a in p.Authors
+                             select new AuthorDTO(a.Id, a.GivenName, a.Surname)
+                         ).ToHashSet(),
+                         p.URL,
+                         p.Abstract
+                     );
+        return await papers.FirstOrDefaultAsync();
     }
 }
