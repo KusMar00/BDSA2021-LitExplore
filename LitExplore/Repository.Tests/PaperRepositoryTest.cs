2cd4bd781fe1c84a56c3419a0091305896b5dd77
Magnus Dalsgaard Larsen <45166031+BlueMango10@users.noreply.github.com>
2021-12-03 13:00:17 +0100

    Feature/repository (#10)
    * Complete migrations/code-first database creation
    
    * Setup test. (Has issues with sqlite)
    
    * Fix test setup
    
    * Fix Repository test initiation again
    
    * Disabled the seed test
    
    * Repository Structure
    
    * Improve test setup
    
    * More test improvement
    
    * Begun work on paperRepository
    
    * Implement PaperRepository.ReadAsync
    
    Co-authored-by: Magnus Dalsgaard Larsen <daml@itu.dk>

--- LitExplore/Repository.Tests/PaperRepositoryTest.cs
+++ LitExplore/Repository.Tests/PaperRepositoryTest.cs
@@ -8,7 +8,40 @@ public class PaperRepositoryTest : RepositoryTests
 
     protected override void SeedDatabase()
     {
-        // Nothing for now
+        Author
+            Author_1 = new Author { Id = 1, GivenName = "Author 1", Surname = "Surname 1" },
+            Author_2 = new Author { Id = 2, GivenName = "Author 2", Surname = "Surname 2" },
+            Author_3 = new Author { Id = 3, GivenName = "Author 3", Surname = "Surname 3" },
+            Author_4 = new Author { Id = 4, GivenName = "Author 4", Surname = "Surname 4" },
+            Author_5 = new Author { Id = 5, GivenName = "Author 5", Surname = "Surname 5" };
+
+        Paper
+            Paper_1 = new Paper
+            {
+                Id = 0,
+                Authors = new HashSet<Author> { Author_1, Author_2 },
+                Name = "Paper 1",
+                Abstract = "Abstract 1"
+            },
+            Paper_2 = new Paper
+            {
+                Id = 1,
+                Authors = new HashSet<Author> { Author_3, Author_4 },
+                Name = "Paper 2",
+                Abstract = "Abstract 2"
+            },
+            Paper_3 = new Paper
+            {
+                Id = 1,
+                Authors = new HashSet<Author> { Author_5 },
+                Name = "Paper 3",
+                Abstract = "Abstract 3",
+                Citings = new[] { Paper_1, Paper_2 }
+            };
+
+        Context.Papers.AddRange(new[] { Paper_1 });
+
+        Context.SaveChanges();
     }
 
     [Fact]
@@ -19,6 +52,19 @@ public class PaperRepositoryTest : RepositoryTests
 
         // Act
         var actual = await repo.ReadAsync(10);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async void Read_Paper_1_Returns_Paper_1()
+    {
+        // Arrange
+        PaperDTO expected = new(1, "Paper 1");
+
+        // Act
+        var actual = await repo.ReadAsync(1);
 
         // Assert
         Assert.Equal(expected, actual);
--- LitExplore/Repository/GlobalUsings.cs
+++ LitExplore/Repository/GlobalUsings.cs
@@ -1,12 +1,9 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using System.Text;
 global using System.Threading.Tasks;
 
-global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.EntityFrameworkCore;
 
 global using LitExplore.Repository.Entities;
