5a9992c035fda0a71e3197d51d028ba2451c2e54
Markus <mkjo@itu.dk>
2021-12-03 12:39:39 +0100

    add basic user-interface

--- LitExplore/Client/Program.cs
+++ LitExplore/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LitExplore.Client;
+using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,8 @@ builder.Services.AddHttpClient("LitExplore.ServerAPI", client => client.BaseAddr
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("LitExplore.ServerAPI"));
+
+builder.Services.AddBootstrapCss();
 
 builder.Services.AddMsalAuthentication(options =>
 {
