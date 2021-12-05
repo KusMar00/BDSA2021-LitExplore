3165024e946ea73418611bb7bc9c0ca6e393b148
Magnus Emil Gervig Svensson <mesv@itu.dk>
2021-12-05 15:10:25 +0100

    UserRepository and .Test complete

--- LitExplore/Repository/IUserRepository.cs
+++ LitExplore/Repository/IUserRepository.cs
@@ -3,6 +3,6 @@
 public interface IUserRepository
 {
     Task<(Status, UserDTO)> CreateAsync(UserDTO user);
-    Task<UserDTO> ReadAsync(Guid id);
+    Task<UserDTO?> ReadAsync(Guid id);
     Task<Status> DeleteAsync(Guid id);
 }
