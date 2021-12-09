23f44d16271dc154ac9ddb7e09742b241955f525
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-12-09 16:15:53 +0100

    disable a test

--- LitExplore/Repository.Tests/DatabaseTests.cs
+++ LitExplore/Repository.Tests/DatabaseTests.cs
@@ -5,7 +5,7 @@ namespace LitExplore.Repository.Tests;
 public class DatabaseTests
 {
     // Enable this test and run it to seed the database without running the whole program. 
-    [Fact/*(Skip = "We don't want to modify the live database when testing.")*/]
+    [Fact(Skip = "We don't want to modify the live database when testing.")]
     public void SeedTest()
     {
         File.WriteAllText("appsettings.json", "{}");
