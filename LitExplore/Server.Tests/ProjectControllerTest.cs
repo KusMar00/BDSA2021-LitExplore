d5c324ae6bb585fec2e6364b7b0690461ed717fc
KalleWil <kalle-wil@hotmail.com>
2021-12-20 17:29:57 +0100

    Updated tests

--- LitExplore/Server.Tests/ProjectControllerTest.cs
+++ LitExplore/Server.Tests/ProjectControllerTest.cs
@@ -42,7 +42,8 @@ namespace LitExplore.Server.Tests;
             User
                 User_1 = new User {Id = guid_1, DisplayName = "User 1" },
                 User_2 = new User {Id = guid_2, DisplayName = "User 2" },
-                User_3 = new User {Id = guid_3, DisplayName = "User 3" };
+                User_3 = new User {Id = guid_3, DisplayName = "User 3" },
+                User_4 = new User {Id = guid_4, DisplayName = "User 4" };
 
             Project 
                 Project_1 = new Project 
@@ -71,6 +72,7 @@ namespace LitExplore.Server.Tests;
                 };
                 
                 Context.Projects.AddRange(new[] {Project_1, Project_2, Project_3});
+                Context.Users.Add(User_4);
                 Context.SaveChanges();
         }
 
@@ -252,4 +254,113 @@ namespace LitExplore.Server.Tests;
             Assert.Equal(expected, actual);
         }
         #endregion
+
+        [Fact]
+        public async void Get_UserProjectDTO_By_userId_4_Returns_Empty_UserProjectDTO()
+        {
+            // Arrange
+            var expected = new UserProjectDTO(Id: guid_4, 
+                                            Owns: new List<ProjectDTO>(){ }.AsReadOnly(), 
+                                            HasAccesTo: new List<ProjectDTO>(){ }.AsReadOnly()
+                                            );
+
+            // Act
+            var actual = await projectController.Get(guid_4);
+        
+            // Assert
+            #pragma warning disable CS8602 // Dereference of a possibly null reference.
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Owns, actual.Owns);
+            Assert.Equal(expected.HasAccesTo, actual.HasAccesTo);
+        }
+
+        [Fact]
+        public async void ReadProjectsByUserAsync_User_Null_Returns_Empty_UserProjectDTO()
+        {
+            // Arrange
+            var expected = new UserProjectDTO(Id: Guid.Empty, 
+                                            Owns: new List<ProjectDTO>(){ }.AsReadOnly(), 
+                                            HasAccesTo: new List<ProjectDTO>(){ }.AsReadOnly()
+                                            );
+
+            // Act
+            User? testUser = new User(Guid.Empty, "");
+            var actual = await projectController.Get(testUser.Id);
+        
+            // Assert
+            #pragma warning disable CS8602 // Dereference of a possibly null reference.
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Owns, actual.Owns);
+            Assert.Equal(expected.HasAccesTo, actual.HasAccesTo);
+        }
+        
+    private void AssertProjectDTOEquals (ProjectDTO? expected, ProjectDTO? actual)
+    {
+        Assert.NotNull(actual);
+        #pragma warning disable CS8602 // Dereference of a possibly null reference.
+        Assert.Equal(expected.Id, actual.Id);
+        #pragma warning restore CS8602 // Dereference of a possibly null reference.
+        Assert.Equal(expected.Name, actual.Name);
+        Assert.Equal(expected.Owner, actual.Owner);
+        Assert.Equal(expected.Collaborators, actual.Collaborators);
     }
+    [Fact]
+    public async void Post_Creates_Project_And_Returns_Created()
+    {
+        // Arrange
+        var expectedStatus = Status.Created;
+        var expectedProject = new ProjectDTO(
+            Id: 4,
+            Name: "Project 4",
+            Owner: new(guid_3, "User 3"),
+            Collaborators: new HashSet<UserDTO>()
+        );
+
+        // Act
+         var actual = await projectController.Post(new ProjectCreateDTO(guid_3, "Project 4"));
+
+        // Assert
+        Assert.Equal(expectedStatus, actual.Item1);
+        AssertProjectDTOEquals(expectedProject, actual.Item2);
+        // Was the project actually created?
+        AssertProjectDTOEquals(expectedProject, await projectController.Get(4));
+    }
+
+    [Fact]
+    public async void Post_Non_Existing_User_Returns_NotFound()
+    {
+        // Arrange
+        var expectedStatus = Status.NotFound;
+
+        // Act
+        var actual = await projectController.Post(new ProjectCreateDTO(Guid.Empty, "Project 5"));
+
+        // Assert
+        Assert.Equal(expectedStatus, actual.Item1);
+        Assert.Null(actual.Item2);
+        Assert.Null(await repo.ReadProjectAsync(5));
+    }
+
+    [Fact]
+    public async void CreateProjectAsync_Name_Exists_For_Owner_Conflict()
+    {
+        // Arrange
+        var expectedStatus = Status.Conflict;
+        var expectedProject = new ProjectDTO(
+            Id: 1,
+            Name: "Project 1",
+            Owner: new UserDTO(guid_1, "User 1"),
+            Collaborators: new HashSet<UserDTO>()
+            {
+                new UserDTO(guid_2, "User 2"),
+            }
+        );
+
+        // Act
+        var actual = await projectController.Post(new ProjectCreateDTO(guid_1, "Project 1"));
+
+        // Assert
+        Assert.Equal(expectedStatus, actual.Item1);
+        AssertProjectDTOEquals(expectedProject, actual.Item2);
+    }   
+}
