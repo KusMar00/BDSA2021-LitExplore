0e373a4afcb6437249df57993ab5fcdb61b4b000
Markus Johansen <74660130+KusMar00@users.noreply.github.com>
2021-12-21 23:43:06 +0100

    Delete LitExplore directory

--- LitExplore/Repository.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-
-global using Microsoft.Data.Sqlite;
-global using Microsoft.EntityFrameworkCore;
-global using Xunit;
-
-global using LitExplore.Repository.Entities;
